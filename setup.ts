cb6c766afa9cf4289cc3f41b8c2eccad42ab9c9a
Fran Zekan <zekan.fran369@gmail.com>
2020-03-30 00:43:53 +0200

    Add precommit hook for copy to template

--- setup.ts
+++ setup.ts
@@ -3,8 +3,7 @@ import * as path from "path";
 
 const exclude = ["yarn.lock", "yarn-error.log", "package.json", "node_modules"];
 
-const filter = (files: fs.Dirent[]) =>
-  files.filter(item => !exclude.includes(item.name));
+const filter = (files: fs.Dirent[]) => files.filter((item) => !exclude.includes(item.name));
 
 const genPath = (base: string, name: string) => path.join(base, name);
 
@@ -15,7 +14,7 @@ const copy = (from: string, to: string) => {
       return;
     }
 
-    filter(files).forEach(file => {
+    filter(files).forEach((file) => {
       if (file.isDirectory()) {
         fs.mkdirSync(genPath(to, file.name));
         copy(genPath(from, file.name), genPath(to, file.name));
@@ -26,16 +25,14 @@ const copy = (from: string, to: string) => {
   });
 };
 
+console.log("Coping to template folder 👨‍💻");
+
 fs.rmdir("./template", { recursive: true }, () => {
   fs.mkdir("template", () => {
     copy("./src", "./template");
   });
 
-  const {
-    devDependencies,
-    dependencies,
-    scripts
-  } = require("./src/package.json");
+  const { devDependencies, dependencies, scripts } = require("./src/package.json");
 
   const data = { devDependencies, dependencies, scripts };
 
@@ -45,3 +42,5 @@ fs.rmdir("./template", { recursive: true }, () => {
     console.log("done with file");
   });
 });
+
+console.log("Copied to template folder 🎉");
