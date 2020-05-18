7bc12d1a957d166868f4e587be782420c2926b05
Fran Zekan <zekan.fran369@gmail.com>
2020-05-18 23:35:41 +0200

    Add 404 page

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,13 +3,15 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Page from "./pages/Page";
+import NotFound from "./pages/NotFound";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/page" component={Page} exact />
-        <Route path="/" component={Home} />
+        <Route path="/" component={Home} exact />
+        <Route path="/" component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
