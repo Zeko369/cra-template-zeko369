c92261065f514d6a085e378859172ae5b8a6c7a3
Fran Zekan <zekan.fran369@gmail.com>
2020-05-26 10:55:47 +0200

    Finish removing styled-component, update base

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Button from "../components/Button";
-import { Input } from "@chakra-ui/core";
+import { Input, Button, Stack } from "@chakra-ui/core";
 
 const Home: React.FC = () => {
   const [count, setCount] = React.useState(0);
 
   return (
-    <div>
+    <Stack>
       <Link to="/page">Page</Link>
       <br />
       <i className="material-icons">delete</i>
       <Button onClick={(): void => setCount((v) => v + 1)}>This is a button: {count}</Button>
       <Input />
-    </div>
+    </Stack>
   );
 };
 
